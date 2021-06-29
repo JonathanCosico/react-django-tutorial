e3ab534935575c480a9153b92dc1a21823164df5
JonathanCosico <jcosico@uci.edu>
2021-06-29 12:10:58 -0700

    React Router and Building Components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,27 @@
 import React, { Component, component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
+
+// takes component and renders in the app and displays on the page
 export default class App extends Component {
     constructor(props) {
         super(props);
+        // this.state = {} whenever this edited will rerender our components
     }
     
     render() {
-        return (<h1>Testing React Code</h1>)
+        return (
+            <div>
+                <HomePage />
+            </div>
+        ); // can pass props into home page now
     }
 }
+// to embed JS code into html text, use {}
+// e.g. {this.props.name}
 
 const appDiv = document.getElementById("app");
-render(<App/>, appDiv)
+
+// can pass props into app
+render(<App />, appDiv)
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import index
 
+# when adding URLs, need to add to both this file and Django
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('join', index),
+    path('create', index),
+    path('join/1', index)
 ]
