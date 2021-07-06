6f584717ae2d2f74956df07a6e2211ff35623161
JonathanCosico <jcosico@uci.edu>
2021-07-05 17:26:07 -0700

    Handling POST Requests (backend)

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,10 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id','code','host','guest_can_pause','votes_to_skip','created_at')
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    # make sure data sent in post req fits to create new room eg. /create-room {votes_to_skip: 3, }
+    # when handling different request, good to user serializer
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
