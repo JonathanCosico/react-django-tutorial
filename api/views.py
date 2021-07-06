7847bf3809e1c8a14c2f8b428e63d29cd7bd9e73
JonathanCosico <jcosico@uci.edu>
2021-07-05 17:25:38 -0700

    Handling POST Requests (backend)

--- api/views.py
+++ api/views.py
@@ -1,12 +1,47 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 # holds all endpoints e.g website.com/endpoint
 
+# when connecting to a site, use a session
+# which is a temporary connection btwn 2 devices
+# works like how you can access the same sites on the same devices
+# bc you're on the same "session" -> doesn't need you to resign-in
+# session ID'd by unique key
+
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    # API view allows us to overwrite get, post methods
+    serializer_class = CreateRoomSerializer 
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        serializer = self.seralizer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists(): # if updating room
+                room = queryset[0]
+                room.quest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_paus', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+            else: # creating new room
+                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+                # still need to return response that says it's valid or not
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
